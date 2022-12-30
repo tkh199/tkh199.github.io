ff1d03b0a6014bc101509b4527fee1f411a47805
OHIN A. Landry <54063351+Landryohin@users.noreply.github.com>
2022-12-30 15:26:07 +0100

    4push of 30/12/22

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1508,6 +1508,7 @@ window.particlesJS = function(tag_id, params){
   /* set size canvas */
   canvas_el.style.width = "100%";
   canvas_el.style.height = "100%";
+  canvas_el.style.marginTop = "-120px";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
